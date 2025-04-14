e8fc478c9111242964005f7569b59d22d5a2d0b3
Vu Phan Quang (SDC11) <vu.phan@ntq-solution.com.vn>
2025-04-14 16:07:58 +0700

    update

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "*", // or specify the allowed origins
+    origin: "https://fe-rouge-kappa.vercel.app", // or specify the allowed origins
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     // allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
     // credentials: true, // enable cookies if needed
