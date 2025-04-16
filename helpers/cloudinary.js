db43887d1dcd55a617a3c0c2b026755b5785d2fa
Vu Phan Quang (SDC11) <vu.phan@ntq-solution.com.vn>
2025-04-16 14:33:23 +0700

    update

--- helpers/cloudinary.js
+++ helpers/cloudinary.js
@@ -1,10 +1,11 @@
+require('dotenv').config()
 const cloudinary = require("cloudinary").v2;
 const multer = require("multer");
 
 cloudinary.config({
-  cloud_name: "",
-  api_key: "",
-  api_secret: "",
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const storage = new multer.memoryStorage();
